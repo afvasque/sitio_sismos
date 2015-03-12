cee6759959c7801bc98928c0b249e80cafbcd966
afvasque <avasquezg@gmail.com>
2015-03-12 08:57:08 -0300

    Pushpins funcionando

--- Sismos/Controllers/HomeController.cs
+++ Sismos/Controllers/HomeController.cs
@@ -1,15 +1,77 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace Sismos.Controllers
 {
+    public static class JavaScript
+    {
+        public static string Serialize(object o)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            return js.Serialize(o);
+        }
+    }
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            string[] pinsLat = new string[20];
+            string[] pinsLong = new string[20];
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.csv");
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            string results = sr.ReadToEnd();
+            sr.Close();
+
+            string[] tempStr = results.Replace('\n', '\r').Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string[] tempStr2 = new string[tempStr.Length-1];
+
+            for(int i = 1; i < tempStr.Length; i++)
+            {
+                tempStr2[i - 1] = tempStr[i]; 
+            }
+
+
+            int num_rows = 20;
+            int num_cols = 4;
+
+            string[,] data = new string[num_rows, num_cols];
+
+            for (int r = 0; r < num_rows; r++)
+            {
+                string[] line_r = tempStr2[r].Split(',');
+                for (int c = 0; c < num_cols; c++)
+                {
+                    if(c == 3){
+                        c = 4;
+                        data[r, c-1] = line_r[c];
+                    }
+                    else
+                    {
+                        data[r, c] = line_r[c];
+                    }
+                    
+
+                }
+
+                pinsLat[r] = data[r, 1];
+                pinsLong[r] = data[r, 2];
+            }
+
+            ViewData["pinLat"] = Sismos.Controllers.JavaScript.Serialize(pinsLat);
+            ViewData["pinLong"] = Sismos.Controllers.JavaScript.Serialize(pinsLong);
+
             return View();
         }
 
